e3501aa50f2ec8771f02adab65d5f9cf1aafc1b6
neveditsin <neveditsin@gmail.com>
2017-01-09 14:45:38 -0400

    dev: LocalExec state draft

---
+++ src/main/java/com/nc/action/LocalExec.java
@@ -0,0 +1,88 @@
+package com.nc.action;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+
+import com.nc.events.Event;
+import com.nc.events.Event.EventType;
+import com.nc.utils.GlobalLogger;
+
+public class LocalExec implements Action {	
+	private final String cmd;
+	private final Long timeout;
+	private final Long forceDestroyIn;
+	private final boolean isDaemon;
+	
+	public LocalExec(String cmd, Long timeout, Long forceDestroyIn, boolean isDaemon) {
+		super();
+		this.cmd = cmd;
+		this.timeout = timeout;
+		this.forceDestroyIn = forceDestroyIn;
+		this.isDaemon = isDaemon;
+	}
+
+	@Override
+	public Event exec() {        
+        try {
+        	Process pr = Runtime.getRuntime().exec(cmd);
+        	
+			if (isDaemon) {
+				// do not check ret and do not wait for termination
+				return new Event(EventType.SUCCESS);
+			}
+			
+			if (forceDestroyIn > 0){
+				// do not check ret, just destroy the process
+				Thread.sleep(forceDestroyIn);
+				pr.destroyForcibly();
+				return new Event(EventType.SUCCESS);
+			}
+        	
+        	
+			if (timeout > 0) {
+				if (pr.waitFor(timeout, TimeUnit.MILLISECONDS) == false) {
+					Process dp = pr.destroyForcibly();
+					if (dp.isAlive()) {
+						GlobalLogger
+								.warning("Process ["
+										+ cmd
+										+ "] + could not have been terminated. Please terminate the process manually");
+					}
+					return new Event(EventType.FAILURE, "timeout");
+				}
+			}
+
+			int ret = pr.waitFor();
+			
+			if (ret == 0) {
+				return new Event(EventType.SUCCESS,
+						streamToString(pr.getInputStream()));
+			} else {
+				return new Event(EventType.FAILURE,
+						streamToString(pr.getErrorStream()));
+			}
+
+		} catch (Exception e) {
+			return new Event(EventType.EXCEPTION, e.toString());
+		}        
+	}
+	
+	private static String streamToString(InputStream is) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));            
+        StringBuilder sb = new StringBuilder();
+		String line;
+        while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		return sb.toString();			
+	}
+	
+	public static void main(String[] args){
+		//TODO check
+	}
+
+
+}
